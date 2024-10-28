f4ca10a0dd894c2c3bd715f20631e9d55dd8fb22
dansniper888 <dansniper888@gmail.com>
2024-10-28 21:10:20 +0700

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,18 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99>9.98 and 1000!=1000.1, )
+
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+#4th program
+digit = "123.456"
+digit = float(digit)
+digit = digit * 10
+digit = int(digit)
+digit = digit % 10
+print (digit)
